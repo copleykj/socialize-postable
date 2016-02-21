c03565d24937cfe4324aeb13e06d9613ce7149de
copleykj <copleykj@gmail.com>
2016-02-20 23:45:05 -0500

    patch version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "socialize:feed",
     summary: "A package for impmementing a social network style news feed",
-    version: "0.2.1",
+    version: "0.2.2",
 });
 
 Package.onUse(function(api) {
