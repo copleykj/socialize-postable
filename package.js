bd9ffc413e33af86f98634cfdd86addeaabda82a
Kelly Copley <copleykj@gmail.com>
2017-08-10 07:56:16 -0400

    fix linting issues in package.js

--- package.js
+++ package.js
@@ -1,23 +1,24 @@
+
 Package.describe({
-    name: "socialize:postable",
-    summary: "A package to create models which can be posted upon",
-    version: "1.0.0",
-    git: "https://github.com/copleykj/socialize-postable.git"
+    name: 'socialize:postable',
+    summary: 'A package to create models which can be posted upon',
+    version: '1.0.0',
+    git: 'https://github.com/copleykj/socialize-postable.git',
 });
 
-Package.onUse(function(api) {
-    api.versionsFrom("1.3");
+Package.onUse(function (api) {
+    api.versionsFrom('1.3');
 
-    api.use("ecmascript");
+    api.use('ecmascript');
 
     api.use([
-        "socialize:commentable@1.0.0"
+        'socialize:commentable@1.0.0',
     ]);
 
     api.mainModule('server.js', 'server');
     api.mainModule('common.js');
 });
 
-Package.onTest(function(api){
-    //write some tests please!
+Package.onTest(function (api) {
+    // write some tests please!
 });
