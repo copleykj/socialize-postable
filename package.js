4ca5a58d7f8c3b57d93375fdf4eed3d222f1c7e9
Kelly Copley <copleykj@gmail.com>
2021-06-27 23:55:52 -0400

    update deps and versionsFrom for compatibility

--- package.js
+++ package.js
@@ -2,17 +2,17 @@
 Package.describe({
     name: 'socialize:postable',
     summary: 'A package to create models which can be posted upon',
-    version: '1.0.3',
+    version: '1.0.4',
     git: 'https://github.com/copleykj/socialize-postable.git',
 });
 
 Package.onUse(function _(api) {
-    api.versionsFrom('1.10.2');
+    api.versionsFrom(['1.10.2', '2.3']);
 
     api.use('ecmascript');
 
     api.use([
-        'socialize:commentable@1.0.4',
+        'socialize:commentable@1.0.5',
     ]);
 
     api.mainModule('./server/server.js', 'server');
